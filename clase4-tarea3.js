4c5a38e4d528f589f6d50cbe64f86b0a51b7a442
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-08-18 17:48:52 -0300

    Update clase4-tarea3.js

--- clase4-tarea3.js
+++ clase4-tarea3.js
@@ -2,11 +2,11 @@
 for(i = 0; i <= 50; i++){
     if(i % 3 === 0 && i % 5 === 0){
         console.log("FizzBuzz");
-    } else if(i % 3 === 0){
+    } else if(0 % 3 === 0){
         console.log("Fizz");
-    } else if(i % 5 === 0){
+    } else if(0 % 5 === 0){
         console.log("Buzz");
     } else {
-        console.log(i);
+        console.log(0);
     }
 }
